5861ae64860b472d750a0c5f9fc62886ac81eb8d
Arfaoui11 <72455724+Arfaoui11@users.noreply.github.com>
2023-01-17 23:26:02 +0100

    new update notifications and chat with websocket 1/17/2023

--- src/app/rapports/footer/footer.component.ts
+++ src/app/rapports/footer/footer.component.ts
@@ -1,10 +1,50 @@
-import { Component } from '@angular/core';
+import {Component, HostListener, OnDestroy, OnInit} from '@angular/core';
+import {WebsocketService} from "../../services/websocket.service";
+import {Message} from "../../model/Message";
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
-  styleUrls: ['./footer.component.css']
+  styleUrls: ['./footer.component.scss']
 })
-export class FooterComponent {
+export class FooterComponent implements OnInit,OnDestroy {
+  websocket: WebSocket;
+  message: string = '';
+  constructor(private websocketService: WebsocketService) {
+
+    this.websocket = this.websocketService.createNew();
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  ngOnDestroy() {
+    this.websocket.close();
+  }
+
+  sendNotificaion() {
+    let msg = this.message;
+    if (msg == '' || msg == undefined) return;
+    let message: Message = {
+      type: 'NOTIFICATIONS',
+      from: 2,
+      fromUserName: 'arfaoui new',
+      message: msg
+    };
+    this.websocket.send(JSON.stringify(message));
+  }
+
+  @HostListener('window:beforeunload')
+  close() {
+    let message: Message = {
+      type: 'LEFT',
+      from: 1,
+      fromUserName: 'mahdi',
+      message: ""
+    }
+    this.websocket.send(JSON.stringify(message));
+  }
 
 }
--- src/app/rapports/rapports.module.ts
+++ src/app/rapports/rapports.module.ts
@@ -3,13 +3,21 @@ import { CommonModule } from '@angular/common';
 
 import { RapportsRoutingModule } from './rapports-routing.module';
 import {FooterComponent} from "./footer/footer.component";
+import {DocumentsModule} from "../documents/documents.module";
+import {FormsModule, ReactiveFormsModule} from "@angular/forms";
+import {HttpClientModule} from "@angular/common/http";
 
 
 @NgModule({
   declarations: [FooterComponent],
   imports: [
     CommonModule,
-    RapportsRoutingModule
+    RapportsRoutingModule,
+    DocumentsModule,
+    FormsModule,
+    ReactiveFormsModule,
+    HttpClientModule,
+    DocumentsModule
   ]
 })
 export class RapportsModule { }
